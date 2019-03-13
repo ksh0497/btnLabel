77198183b5bc0b01e2b0e25f1b033be1eb4fa9e1
D7703_29 <d7703_29@d7703-29ui-imac.local>
2019-03-13 16:31:54 +0900

    0313

--- Button01/ViewController.swift
+++ Button01/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var UILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
     }
-
-
+    @IBAction func leftbtn(_ sender: UIButton) {
+        UILabel.text="Left Button Pressod!!"
+    }
+    
+    @IBAction func rightbtn(_ sender: UIButton) {
+        UILabel.text="Right Button Pressod!!"
+    }
 }
 
